6b7ba822ed9b4dafc8cf8232de59e3fff01ef7ab
Harsh Gupta <harshgupta102001@gmail.com>
2022-09-04 17:27:13 +0530

    created rooms and added username functionality partially

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,6 +11,9 @@ const $messages = document.querySelector('#messages')
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-message-template').innerHTML
 
+//Options
+const {username , room}=Qs.parse(location.search, {ignoreQueryPrefix: true})
+
 socket.on('message', (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate, {
@@ -64,3 +67,6 @@ $sendLocationButton.addEventListener('click', () => {
         })
     })
 })
+
+
+socket.emit('join',{username , room})
--- src/index.js
+++ src/index.js
@@ -19,8 +19,13 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New websocket connection')
 
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+    socket.on('join',({username, room})=>{
+        socket.join(room)
+
+        socket.emit('message', generateMessage('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+
+    })
 
     socket.on('sendMessage', (message, callback) => {
         io.emit('message', generateMessage(message))
